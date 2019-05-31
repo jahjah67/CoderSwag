2ec7414110163235092ddb75467d005a2767072a
Chang <changszehong93@gmail.com>
2019-05-31 09:50:51 +0800

    RecyclerView for both category and product completed

--- app/src/main/java/com/example/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/com/example/coderswag/Controller/ProductsActivity.kt
@@ -1,16 +1,36 @@
 package com.example.coderswag.Controller
 
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import android.widget.Adapter
+import com.example.coderswag.Adapters.ProductAdapter
 import com.example.coderswag.R
+import com.example.coderswag.Services.DataService
 import com.example.coderswag.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter: ProductAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
+
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType)
+        adapter = ProductAdapter(this, DataService.getProducts(categoryType))
+
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+        if(orientation == Configuration.ORIENTATION_LANDSCAPE){
+            spanCount = 3
+        }
+
+
+        val layoutManager = GridLayoutManager(this, spanCount)
+        productsListView.layoutManager = layoutManager
+        productsListView.adapter = adapter
     }
 }
--- app/src/main/java/com/example/coderswag/Services/DataService.kt
+++ app/src/main/java/com/example/coderswag/Services/DataService.kt
@@ -18,6 +18,10 @@ object DataService {
         Product("Devslopes Graphic Beanie", "$18", "hat1"),
         Product("Devslopes Hat Black", "$20", "hat2"),
         Product("Devslopes Hat Whitee", "$18", "hat3"),
+        Product("Devslopes Hat Snapback", "$22", "hat4"),
+        Product("Devslopes Graphic Beanie", "$18", "hat1"),
+        Product("Devslopes Hat Black", "$20", "hat2"),
+        Product("Devslopes Hat Whitee", "$18", "hat3"),
         Product("Devslopes Hat Snapback", "$22", "hat4")
     )
 
@@ -25,10 +29,19 @@ object DataService {
         Product("Devslopes Hoodie Gray","$28","hoodie1"),
         Product("Devslopes Hoodie Red","$30","hoodie2"),
         Product("Devslopes Gray Hoodie","$28","hoodie3"),
+        Product("Devslopes Black Hoodie","$32","hoodie4"),
+        Product("Devslopes Hoodie Gray","$28","hoodie1"),
+        Product("Devslopes Hoodie Red","$30","hoodie2"),
+        Product("Devslopes Gray Hoodie","$28","hoodie3"),
         Product("Devslopes Black Hoodie","$32","hoodie4")
     )
 
     val shirts = listOf(
+        Product("Devslopes Shirt Black","$18","shirt1"),
+        Product("Devslopes Badge Light Gray","$20","shirt2"),
+        Product("Devslopes Logo Shirt Red","$22","shirt3"),
+        Product("Devslopes Hustle","$22","shirt4"),
+        Product("Kickflip Studio","$18","shirt5"),
         Product("Devslopes Shirt Black","$18","shirt1"),
         Product("Devslopes Badge Light Gray","$20","shirt2"),
         Product("Devslopes Logo Shirt Red","$22","shirt3"),
